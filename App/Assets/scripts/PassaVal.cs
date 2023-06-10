39ab45128b9e15f37f00bd8148f9a185821eea7f
VictorLucasLIT <133718091+VictorLucasLIT@users.noreply.github.com>
2023-06-10 09:53:10 -0300

    correção de passa valores

--- App/Assets/scripts/PassaVal.cs
+++ App/Assets/scripts/PassaVal.cs
@@ -12,9 +12,9 @@ public class PassaVal : MonoBehaviour
 
     void Start()
     {
-        valorGtxt.text= NovaTela1.instance1;
-        valorDtxt.text= NovaTela1.instance2;
-        valorHtxt.text= NovaTela1.instance3;
+        valorGtxt.text= PlayerPrefs.GetString("Glicemia");
+        valorDtxt.text= PlayerPrefs.GetString("Data");
+        valorHtxt.text= PlayerPrefs.GetString("Hora");
     }
 
 }
